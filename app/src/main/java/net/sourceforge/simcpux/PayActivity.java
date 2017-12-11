fbe947f28b01e1b4100918ee00110f6342df5c24
zhengcf <zhengcaifu@1bgx.net>
2017-12-11 15:17:37 +0800

    test

--- app/src/main/java/net/sourceforge/simcpux/PayActivity.java
+++ app/src/main/java/net/sourceforge/simcpux/PayActivity.java
@@ -1,82 +1,142 @@
 package net.sourceforge.simcpux;
 
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
 import com.tencent.mm.opensdk.constants.Build;
 import com.tencent.mm.opensdk.modelpay.PayReq;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class PayActivity extends Activity {
-	
-	private IWXAPI api;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.pay);
-		
-		api = WXAPIFactory.createWXAPI(this, "wxb4ba3c02aa476ea1");
+import net.sourceforge.simcpux.http.OkHttpUtil;
 
-		Button appayBtn = (Button) findViewById(R.id.appay_btn);
-		appayBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String url = "http://wxpay.wxutil.com/pub_v2/app/app_pay.php";
-				Button payBtn = (Button) findViewById(R.id.appay_btn);
-				payBtn.setEnabled(false);
-				Toast.makeText(PayActivity.this, "获取订单中...", Toast.LENGTH_SHORT).show();
-		        try{
-					byte[] buf = Util.httpGet(url);
-					if (buf != null && buf.length > 0) {
-						String content = new String(buf);
-						Log.e("get server pay params:",content);
-			        	JSONObject json = new JSONObject(content); 
-						if(null != json && !json.has("retcode") ){
-							PayReq req = new PayReq();
-							//req.appId = "wxf8b4f85f3a794e77";  // 测试用appId
-							req.appId			= json.getString("appid");
-							req.partnerId		= json.getString("partnerid");
-							req.prepayId		= json.getString("prepayid");
-							req.nonceStr		= json.getString("noncestr");
-							req.timeStamp		= json.getString("timestamp");
-							req.packageValue	= json.getString("package");
-							req.sign			= json.getString("sign");
-							req.extData			= "app data"; // optional
-							Toast.makeText(PayActivity.this, "正常调起支付", Toast.LENGTH_SHORT).show();
-							// 在支付之前，如果应用没有注册到微信，应该先调用IWXMsg.registerApp将应用注册到微信
-							api.sendReq(req);
-						}else{
-				        	Log.d("PAY_GET", "返回错误"+json.getString("retmsg"));
-				        	Toast.makeText(PayActivity.this, "返回错误"+json.getString("retmsg"), Toast.LENGTH_SHORT).show();
-						}
-					}else{
-			        	Log.d("PAY_GET", "服务器请求错误");
-			        	Toast.makeText(PayActivity.this, "服务器请求错误", Toast.LENGTH_SHORT).show();
-			        }
-		        }catch(Exception e){
-		        	Log.e("PAY_GET", "异常："+e.getMessage());
-		        	Toast.makeText(PayActivity.this, "异常："+e.getMessage(), Toast.LENGTH_SHORT).show();
-		        }
-		        payBtn.setEnabled(true);
-			}
-		});		
-		Button checkPayBtn = (Button) findViewById(R.id.check_pay_btn);
-		checkPayBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				boolean isPaySupported = api.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT;
-				Toast.makeText(PayActivity.this, String.valueOf(isPaySupported), Toast.LENGTH_SHORT).show();
-			}
-		});
-	}
-	
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class PayActivity extends Activity {
+
+    private IWXAPI api;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay);
+
+        api = WXAPIFactory.createWXAPI(this, "wxb4ba3c02aa476ea1");
+
+        Button appayBtn = (Button) findViewById(R.id.appay_btn);
+        appayBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                payTest();
+            }
+        });
+        Button checkPayBtn = (Button) findViewById(R.id.check_pay_btn);
+        checkPayBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                boolean isPaySupported = api.getWXAppSupportAPI() >= Build.PAY_SUPPORTED_SDK_INT;
+                Toast.makeText(PayActivity.this, String.valueOf(isPaySupported), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void payTest() {
+        OkHttpUtil.getOrderInfo(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                PayActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AlertDialog dialog = new AlertDialog.Builder(PayActivity.this).setMessage(e.getMessage()).create();
+                        dialog.setCancelable(true);
+                        dialog.show();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) {
+                PayActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PayReq req = new PayReq();
+                        try {
+                            JSONObject json = new JSONObject(response.body().string());
+                            //req.appId = "wxf8b4f85f3a794e77";  // 测试用appId
+                            req.appId = json.getString("appid");
+                            req.partnerId = json.getString("partnerid");
+                            req.prepayId = json.getString("prepayid");
+                            req.nonceStr = json.getString("noncestr");
+                            req.timeStamp = json.getString("timestamp");
+                            req.packageValue = json.getString("package");
+                            req.sign = json.getString("sign");
+                            req.extData = "app data"; // optional
+                            Toast.makeText(PayActivity.this, "正常调起支付", Toast.LENGTH_SHORT).show();
+                            // 在支付之前，如果应用没有注册到微信，应该先调用IWXMsg.registerApp将应用注册到微信
+                            api.sendReq(req);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void payDefault() {
+        String url = "http://wxpay.wxutil.com/pub_v2/app/app_pay.php";
+        Toast.makeText(PayActivity.this, "获取订单中...", Toast.LENGTH_SHORT).show();
+        try {
+            byte[] buf = Util.httpGet(url);
+            if (buf != null && buf.length > 0) {
+                String content = new String(buf);
+                Log.e("get server pay params:", content);
+                JSONObject json = new JSONObject(content);
+                if (null != json && !json.has("retcode")) {
+                    PayReq req = new PayReq();
+                    //req.appId = "wxf8b4f85f3a794e77";  // 测试用appId
+                    req.appId = json.getString("appid");
+                    req.partnerId = json.getString("partnerid");
+                    req.prepayId = json.getString("prepayid");
+                    req.nonceStr = json.getString("noncestr");
+                    req.timeStamp = json.getString("timestamp");
+                    req.packageValue = json.getString("package");
+                    req.sign = json.getString("sign");
+                    req.extData = "app data"; // optional
+                    Toast.makeText(PayActivity.this, "正常调起支付", Toast.LENGTH_SHORT).show();
+                    // 在支付之前，如果应用没有注册到微信，应该先调用IWXMsg.registerApp将应用注册到微信
+                    api.sendReq(req);
+                } else {
+                    Log.d("PAY_GET", "返回错误" + json.getString("retmsg"));
+                    Toast.makeText(PayActivity.this, "返回错误" + json.getString("retmsg"), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.d("PAY_GET", "服务器请求错误");
+                Toast.makeText(PayActivity.this, "服务器请求错误", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.e("PAY_GET", "异常：" + e.getMessage());
+            Toast.makeText(PayActivity.this, "异常：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/net/sourceforge/simcpux/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/net/sourceforge/simcpux/wxapi/WXPayEntryActivity.java
@@ -17,40 +17,40 @@ import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
 public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
-	
-	private static final String TAG = "WXPayEntryActivity";
-	
+
+    private static final String TAG = "WXPayEntryActivity";
+
     private IWXAPI api;
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pay_result);
-        
-    	api = WXAPIFactory.createWXAPI(this, Constants.APP_ID);
+
+        api = WXAPIFactory.createWXAPI(this, Constants.APP_ID);
         api.handleIntent(getIntent(), this);
     }
 
-	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-		setIntent(intent);
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
         api.handleIntent(intent, this);
-	}
+    }
 
-	@Override
-	public void onReq(BaseReq req) {
-	}
+    @Override
+    public void onReq(BaseReq req) {
+    }
 
-	@Override
-	public void onResp(BaseResp resp) {
-		Log.d(TAG, "onPayFinish, errCode = " + resp.errCode);
+    @Override
+    public void onResp(BaseResp resp) {
+        Log.d(TAG, "onPayFinish, errCode = " + resp.errCode);
 
-		if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setTitle(R.string.app_tip);
-			builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(resp.errCode)));
-			builder.show();
-		}
-	}
+        if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.app_tip);
+            builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(resp.errCode) + resp.errStr));
+            builder.show();
+        }
+    }
 }
